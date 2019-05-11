65464564b474b300daf69a8464419fa4c55723e1
Feranmi Aboderin <aboderinf@gmail.com>
2019-05-11 22:03:38 +0100

    chore(refactoring): Refactor root route

--- Api/server.js
+++ Api/server.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes/index';
 import dotenv from 'dotenv';
-const Joi = require ('joi');// instantiate validator class Joi
+import routes from './routes/index';
+const Joi = require('joi');
+// instantiate validator class Joi
 dotenv.config();
 // Instantiate express
 const app = express();
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 // Register our routes in app
-app.use('/', routes);
+app.use('/api/v1/', routes);
 // Start our server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
