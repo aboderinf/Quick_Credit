0292c3b723232da4f1427eede1f9d36278df11c3
aboderinf <47601616+aboderinf@users.noreply.github.com>
2019-05-17 11:23:34 +0200

    #166045469 Host app on heroku (#28)
    * chore(coverall): add coverall and coverage script
    
    [#165957289]
    
    * bug (coveralls): fix bug with travis build
    
    [#165957289]
    
    * bug(coveralls): fix travis build bug
    
    [#165957289]
    
    * bug(coveralls): fix travis build
    
    [#165957289]
    
    * bug(travis): fix bug with coveralls
    
    * chore(heroku): add start script
    
    * chore(heroku): add build script
    
    * bug(heroku): fix heroku build bug
    
    * bug(heroku): fix heroku  build error
    
    * bug(Heroku): uninstall babel-cli
    
    Fix heroku build error by uninstalling old babel-cli

--- Api/server.js
+++ Api/server.js
@@ -1,8 +1,10 @@
-/* eslint-disable no-console */
+// /* eslint-disable no-console */
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import routes from './routes/index';
+import cors from 'cors';
+import morgan from 'morgan';
 const Joi = require('joi');
 // instantiate validator class Joi
 dotenv.config();
@@ -14,6 +16,10 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+// logger
+app.use(morgan('dev'));
+// 3rd party middleware
+app.use(cors());
 // Register our routes in app
 app.use('/api/v1', routes);
 // Start our server
