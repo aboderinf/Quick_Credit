343e0e42f353e4d0001ee21928b1da8915f47636
aboderinf <aboderinf@gmail.com>
2019-05-22 02:46:45 +0200

    bug(verify): fix bugs;

--- Api/controllers/newuserController.js
+++ Api/controllers/newuserController.js
@@ -1,86 +1,87 @@
-const jwt = require ('jsonwebtoken');
-import userModel from '../database/users-db';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import bcrypt from 'bcryptjs';
+import userModel from '../database/users-db';
+
+const jwt = require('jsonwebtoken');
 
 class userController {
   // Post a new user
   static createUser(req, res) {
     userModel.create(req.body)
       .then(({ rows }) => {
+        // eslint-disable-next-line no-param-reassign
+        delete (rows[0].password);
+        const token = jwt.sign({ id: rows[0].id }, process.env.JWT_KEY);
         return res.status(201).json({
           status: 201,
           data: {
+            token,
             ...rows[0],
-            token: jwt.sign({
-              id: rows[0].id,
-            }, process.env.JWT_KEY)
           },
           message: 'Your account has sucessfully been created!',
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-        return res.status(400).json({
-            status: 400,
-            error: error,
-          });
-      })
-  }
-  static userLogin (req, res) {
-    const {email} = req.body;
-    userModel.findByEmail(email)
-    .then((result) => {
-      const user = result.rows[0];
-      if (!user) {
-        return res.status(404).json({
-          status: 404,
-          error: 'Invalid email address or password',
         });
-      }
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        const token = jwt.sign({ id: user.id }, process.env.JWT_KEY);
+      })
+      .catch(error => res.status(400).json({
+        status: 400,
+        error,
+      }));
+  }
+
+  static userLogin(req, res) {
+    const { email } = req.body;
+    userModel.findByEmail(email)
+      .then((result) => {
+        const user = result.rows[0];
+        if (!user) {
+          return res.status(404).json({
+            status: 404,
+            error: 'Invalid email address or password',
+          });
+        }
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const token = jwt.sign({ id: user.id }, process.env.JWT_KEY);
+          return res.status(200).json({
+            status: 200,
+            message: 'Login successful',
+            data: {
+              token,
+              ...user,
+            },
+          });
+        }
+        return res.status(401).json({
+          status: 401,
+          error: 'Incorrect login details',
+        });
+      }).catch(error => res.status(401).json({
+        status: 404,
+        error,
+      }));
+  }
+
+  // Verify User
+  // Mark a user as verified
+  static verifyUser(req, res) {
+    const { useremail } = req.params;
+    userModel.findByEmail(useremail)
+      .then((result) => {
+        const user = result.rows[0];
+        if (!user) {
+          return res.status(404).json({
+
+            status: 404,
+            error: 'NOT FOUND',
+            message: 'user record not found',
+          });
+        }
+        userModel.verifyUser(useremail);
+        // eslint-disable-next-line no-param-reassign
+        result.rows[0].status = 'verified';
         return res.status(200).json({
           status: 200,
-          message: 'Login successful',
-          data: {
-            ...user,
-            token,
-          },
+          message: `user status has been updated to ${user.status}`,
         });
-      }
-      return res.status(401).json({
-        status: 401,
-        error: 'Incorrect login details',
       });
-    }).catch( error => res.status(401).json({
-      status: 404,
-      error: 'NOT FOUND',
-    }));
- }
-    // Verify User
-  // Mark a user as verified
-  static verifyUser (req,res){
-    const {useremail} = req.params;
-    userModel.findByEmail(useremail)
-    .then((result) => {
-      const user = result.rows[0];
-      if (!user) {
-        return res.status(404).json({
-        
-          status: 404,
-          error: 'NOT FOUND',
-          message: 'user record not found',
-        });
-      }
-      userModel.verifyUser(useremail);
-      result.rows[0].status = 'verified';
-      res.status(200).json({
-        status: 200,
-        message: 'user status has been updated to ' + user.status,
-      });
-  });
   }
-  
-}    
+}
 export default userController;
-
--- Api/database/connect.js
+++ Api/database/connect.js
@@ -3,19 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let connectionString, db;
+let connectionString; let
+  db;
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   connectionString = 'postgres://minztlpc:884XcE0ybQDtQLQ-GcZ9k7-s0SKxAMZF@raja.db.elephantsql.com:5432/minztlpc';
-} else if(process.env.NODE_ENV === 'test') {
+} else if (process.env.NODE_ENV === 'test') {
   connectionString = 'postgres://xvapmzgq:ZaLoF6HbrUK6WJ0p4blWu0_gZFtJ7VLm@raja.db.elephantsql.com:5432/xvapmzgq';
 } else {
   connectionString = 'postgres://lexkcvmk:4aql30ElY7FLz53IUlVWA2uhat7d7VZv@raja.db.elephantsql.com:5432/lexkcvmk';
 }
 
 db = new Pool({
-  connectionString, 
+  connectionString,
 });
 
-db &&  console.log('connected to db');
+db && console.log('connected to db');
 export default db;
