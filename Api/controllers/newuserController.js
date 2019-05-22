35595441dbaeac0f3307c53d57c290cf37483a9a
aboderinf <47601616+aboderinf@users.noreply.github.com>
2019-05-22 10:18:54 +0100

    #166109320 Admin can verify user endpoint (database)  (#31)
    * chore(database): Set up database
    
    [Finishes #166076111]
    
    * feature(database): user can sign up
    
    [Finishes #166109077]
    
    * feature(signin): user in database can signin
    
    [Finishes #166109227]
    
    * feature(database):add endpoint to verify user
    
    [Finishes #166109320]

--- Api/controllers/newuserController.js
+++ Api/controllers/newuserController.js
@@ -26,7 +26,64 @@ class userController {
           });
       })
   }
-
+  static userLogin (req, res) {
+    const {email} = req.body;
+    userModel.findByEmail(email)
+    .then((result) => {
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(404).json({
+          status: 404,
+          success: false,
+          error: 'Invalid email address or password',
+        });
+      }
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const token = jwt.sign({ id: user.id }, process.env.JWT_KEY);
+        return res.status(200).json({
+          status: 200,
+          success: true,
+          message: 'Login successful',
+          data: {
+            ...user,
+            token,
+          },
+        });
+      }
+      return res.status(401).json({
+        status: 401,
+        error: 'Incorrect login details',
+      });
+    }).catch( error => res.status(401).json({
+      status: 401,
+      error: 'Incorrect login details',
+    }));
+ }
+    // Verify User
+  // Mark a user as verified
+  static verifyUser (req,res){
+    const {useremail} = req.params;
+    userModel.findByEmail(useremail)
+    .then((result) => {
+      const user = result.rows[0];
+      if (!user) {
+        return res.status(404).json({
+        
+          status: 404,
+          error: 'NOT FOUND',
+          message: 'user record not found',
+        });
+      }
+    if (user){
+      user.status = 'verified';
+      res.status(200).json({
+        status: 200,
+        // data: pick (findUser, ['email', 'firstName', 'lastName', 'password', 'address', 'status']),
+        message: 'user status has been updated to ' + user.status,
+      });
+    };
+  });
+  }
   
 }    
 export default userController;
