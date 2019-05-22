1ce49d5175436d0b6be660746fac98f5047aed05
aboderinf <aboderinf@gmail.com>
2019-05-22 11:06:30 +0200

    chore(db): Add PATCH loans- update status endpoint
    [Finishes 166172978]

--- Api/routes/index.js
+++ Api/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import userController from '../controllers/newuserController';
 import loanController from '../controllers/newloanController';
 import LoanController from '../controllers/loanController';
-import Validation from '../middlewares/validation'
+import Validation from '../middlewares/validation';
 
 const routes = Router();
 routes.post('/auth/signup', Validation.userValidator, userController.createUser);
@@ -11,8 +11,7 @@ routes.patch('/users/:useremail/verify', userController.verifyUser);
 routes.get('/loans', loanController.getAllLoans);
 routes.get('/loans/:loanId', loanController.getSingleLoan);
 routes.post('/loans', loanController.createLoan);
-routes.patch('/loans/:loanId', LoanController.updateloanStatus);
+routes.patch('/loans/:loanId', loanController.updateloanStatus);
 routes.post('/loans/:loanId/repayment', Validation.repaymentValidator, LoanController.createRepayment);
 routes.get('/loans/:loanId/repayments', LoanController.getRepaymentHistory);
 export default routes;
-
